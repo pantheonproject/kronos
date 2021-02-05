d453f6dc239094d93f35715c9de878762f99aac3
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-02-06 09:35:09 +1100

    Format to tabs

--- Kronos.NET/Program.cs
+++ Kronos.NET/Program.cs
@@ -8,20 +8,20 @@ using Microsoft.Extensions.Hosting;
 
 namespace PantheonProject.Kronos.NET
 {
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            CreateHostBuilder(args).Build().Run();
-        }
+	public class Program
+	{
+		public static void Main(string[] args)
+		{
+			CreateHostBuilder(args).Build().Run();
+		}
 
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
-    }
+		// Additional configuration is required to successfully run gRPC on macOS.
+		// For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
+		public static IHostBuilder CreateHostBuilder(string[] args) =>
+			Host.CreateDefaultBuilder(args)
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.UseStartup<Startup>();
+				});
+	}
 }
--- Kronos.NET/Services/GreeterService.cs
+++ Kronos.NET/Services/GreeterService.cs
@@ -5,25 +5,25 @@ using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.Extensions.Logging;
 
-namespace PantheonProject 
+namespace PantheonProject
 {
-    namespace Kronos.NET
-    {
-        public class GreeterService : Greeter.GreeterBase
-        {
-            private readonly ILogger<GreeterService> _logger;
-            public GreeterService(ILogger<GreeterService> logger)
-            {
-                _logger = logger;
-            }
+	namespace Kronos.NET
+	{
+		public class GreeterService : Greeter.GreeterBase
+		{
+			private readonly ILogger<GreeterService> _logger;
+			public GreeterService(ILogger<GreeterService> logger)
+			{
+				_logger = logger;
+			}
 
-            public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
-            {
-                return Task.FromResult(new HelloReply
-                {
-                    Message = "Hello " + request.Name
-                });
-            }
-        }
-    }
+			public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+			{
+				return Task.FromResult(new HelloReply
+				{
+					Message = "Hello " + request.Name
+				});
+			}
+		}
+	}
 }
