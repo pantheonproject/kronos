f2a879078b5c1bc4d3b244127cbe46a516db9959
Luke Kennedy <luke.l.kennedy@gmail.com>
2021-02-07 09:24:21 +1100

    Call template main from example main

--- Kronos.NET/ExampleService/Program.cs
+++ Kronos.NET/ExampleService/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using kronos = PantheonProject.Kronos.NET;
 
 namespace ExampleService
 {
@@ -6,7 +7,7 @@ namespace ExampleService
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			kronos.Program.Main(args);
 		}
 	}
 }
